707caea0e5d0648ca5a28b3fb5282127b05c478b
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-25 10:23:55 +0800

    add webapp

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-navbar">
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase navbar-shrink" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">File Converter</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
